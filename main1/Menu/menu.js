15c2fc5b539c3e14fe4e74b4c3791064ee4c4a2f
Oleksandr-Z <oleksandr.zotov@ukr.net>
2016-09-26 22:02:43 +0300

    in process of development
    finished main, art and photo pages

---
+++ main1/Menu/menu.js
@@ -0,0 +1,34 @@
+﻿$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 144) {
+            $('nav.main-nav').addClass('stickytop');
+        }
+        else {
+            $('nav.main-nav').removeClass('stickytop');
+        }
+    });
+    var page = $("html, body");
+
+    $('div.up').click(function(e) {
+        actualScroll = 0;
+        page.on("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove", function(){
+            page.stop();
+        });
+        page.animate({ scrollTop: $(this).position().top }, 800, function(){
+            page.off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
+        });
+
+        return false; 
+    });
+    var actualScroll = 1;
+    $('div.down').click(function () {
+        var temp = $('.scrollTo' + actualScroll++);
+        $.scrollTo(temp, 800, {offset:-110  });
+        if (actualScroll > 4) {
+            actualScroll = 0;
+        }
+    });
+
+
+
+});
